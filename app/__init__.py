760fbbccefce6d3fddf7602c0c3bd26a43d6b4cd
iELectro13 <ejose.acc@gmail.com>
2021-11-15 20:28:35 -0500

    Templates files initialized

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .config import Config
 from .database import db
+from flask_bootstrap import Bootstrap
 
 def create_app():
     app = Flask(__name__)
+    bootstrap = Bootstrap(app)
     app.config.from_object(Config)
     db.init_app(app)
     return app
